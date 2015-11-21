e19fef63442505fac1e3e52082b13dfbd6f6a862
kosuke-takahashi <kosuke.em@gmail.com>
2015-11-21 18:48:04 +0900

    Initial Commit

---
+++ skita/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  skita
+//
+//  Created by 高橋洸介 on 2015/11/21.
+//  Copyright © 2015年 高橋洸介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
